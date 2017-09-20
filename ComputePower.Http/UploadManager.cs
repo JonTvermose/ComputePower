90eeb756d9266dc6df0729519c92ecacb01e6aec
Jon Tvermose Nielsen <jtn@it-minds.dk>
2017-09-20 21:31:30 +0200

    Removed project ComputePower.Files. Added initial file upload method (not tested).

--- ComputePower.Http/UploadManager.cs
+++ ComputePower.Http/UploadManager.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ComputePower.Http
 {
     public class UploadManager
     {
+        public async Task OpenAndUploadFile(string filePath, string url)
+        {
+            var httpClient = new HttpClient();
+            var content = new MultipartFormDataContent();
+
+            using (var fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                content.Add(new StreamContent(fileStream));
+                content.Headers.Add("txt", "1");
+
+                await httpClient.PostAsync(url, content);
+            }
+        }
     }
 }
